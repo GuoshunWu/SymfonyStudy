50515a7dcfe6bb8518d895bd55157ac8e7c3affb
Guoshun Wu <wuxiude2003@126.com>
2014-08-24 20:41:47 +0800

    update controller

--- src/Acme/HelloBundle/Controller/DefaultController.php
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -3,10 +3,16 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
+    {
+        return new Response("Hello, symphony.");
+    }
+
+    public function helloAction($name)
     {
         return $this->render('AcmeHelloBundle:Default:index.html.twig', array('name' => $name));
     }
--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -5,10 +5,12 @@ namespace Acme\HelloBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends Controller
 {
     public function indexAction($name){
-        return new Response("<html><body>Hello '$name'!</body></html>");
+        $now = new \DateTime();
+        return new Response("<html><body>Hello $name at {$now->format('Y-m-d H:i:s')}!</body></html>");
     }
 }
