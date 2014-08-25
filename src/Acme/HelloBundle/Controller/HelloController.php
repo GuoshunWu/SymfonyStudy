b0fcb5f81bbfd2bdfa047e9e0eed16448e850ce6
Guoshun <wuxiude2003@126.com>
2014-08-25 18:24:25 +0800

    update controller
    (cherry picked from commit 50515a7dcfe6bb8518d895bd55157ac8e7c3affb)

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -5,6 +5,7 @@ namespace Acme\HelloBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends Controller
@@ -12,5 +13,13 @@ class HelloController extends Controller
     public function indexAction($name){
         $now = new \DateTime();
         return new Response("<html><body>Hello $name at {$now->format('Y-m-d H:i:s')}!</body></html>");
+    }
+
+    /**
+     * @Route("/hello/{name}", name="acme_hello_show")
+     * @Template()
+    */
+    public function helloAction(Request $request,$name){
+        return array('name'=>$name, "date"=>new \DateTime());
     }
 }
